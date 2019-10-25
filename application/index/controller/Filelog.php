abc4d9f7e5a5fd0eaaa5d83dc0f5a3ee9e31eb70
llllljian <879056228@qq.com>
2019-10-25 16:59:26 +0800

    2019年10月25日16:59:24

---
+++ application/index/controller/Filelog.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace app\index\controller;
+
+use think\Controller;
+use think\Request;
+use think\Db;
+
+class Filelog extends Common
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param \think\Request $request            
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        //
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param int $id            
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        echo $id;exit;
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param int $id            
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param \think\Request $request            
+     * @param int $id            
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        echo $id . "update";
+        exit;
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param int $id            
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+
+    public function list()
+    {
+        $where = array();
+        $limit = input('get.limit');
+        $skip = (input('get.page', 1) - 1) * $limit;
+
+        $create_time = input('get.create_time');
+        if (!empty($create_time)) {
+            $tempTimeS = strtotime($create_time);
+            $tempTimeE = $tempTimeS + 86399;
+            $where[] = ["create_time", ">=", "{$tempTimeS}"];
+            $where[] = ["create_time", "<=", "{$tempTimeE}"];
+        }
+
+        $res = Db::name("file_logs")
+            ->where($where)
+            ->order(['id' => 'desc'])
+            ->limit($skip, $limit)
+            ->select();
+        $countNum = Db::name("file_logs")
+            ->where($where)
+            ->count();
+        $arr = array(
+            'code' => 0,
+            'msg' => '',
+            'count' => $countNum,
+            'data' => $res
+        );
+
+        echo json_encode($arr);
+    }
+}
---
+++ application/index/controller/Regionlog.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace app\index\controller;
+
+use think\Controller;
+use think\Request;
+use think\Db;
+
+class Regionlog extends Common
+{
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * 显示创建资源表单页.
+     *
+     * @return \think\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * 保存新建的资源
+     *
+     * @param \think\Request $request            
+     * @return \think\Response
+     */
+    public function save(Request $request)
+    {
+        //
+    }
+
+    /**
+     * 显示指定的资源
+     *
+     * @param int $id            
+     * @return \think\Response
+     */
+    public function read($id)
+    {
+        echo $id;exit;
+    }
+
+    /**
+     * 显示编辑资源表单页.
+     *
+     * @param int $id            
+     * @return \think\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * 保存更新的资源
+     *
+     * @param \think\Request $request            
+     * @param int $id            
+     * @return \think\Response
+     */
+    public function update(Request $request, $id)
+    {
+        echo $id . "update";
+        exit;
+    }
+
+    /**
+     * 删除指定资源
+     *
+     * @param int $id            
+     * @return \think\Response
+     */
+    public function delete($id)
+    {
+        //
+    }
+
+    public function list()
+    {
+        $where = array();
+        $limit = input('get.limit');
+        $skip = (input('get.page', 1) - 1) * $limit;
+
+        $res = Db::name("region_log")
+            ->where($where)
+            ->order(['id' => 'desc'])
+            ->limit($skip, $limit)
+            ->select();
+        $countNum = Db::name("region_log")
+            ->where($where)
+            ->count();
+        $arr = array(
+            'code' => 0,
+            'msg' => '',
+            'count' => $countNum,
+            'data' => $res
+        );
+
+        echo json_encode($arr);
+    }
+}
